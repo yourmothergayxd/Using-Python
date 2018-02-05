39a39a8917bba3a933d51f56ecd154dc952b08a9
yourmothergayxd <34488906+yourmothergayxd@users.noreply.github.com>
2018-02-05 12:31:46 -0700

    Add files via upload

---
+++ python_lesson_pay_rate.py
@@ -0,0 +1,4 @@
+import decimal
+rate = float(input("Enter your pay rate: "))
+hours = float(input("Enter your hours: "))
+print (rate * hours)
---
+++ python_lesson_temprature_converter.py
@@ -0,0 +1,2 @@
+c = float(input("Input the temprature in Celcius: "))
+print(1.8 * c + 32)
